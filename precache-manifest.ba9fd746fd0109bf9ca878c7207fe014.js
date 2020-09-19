ccb79315c86d9b70aa5a797e0b911edeace10b89
John Ottenlips <jottenlips@gmail.com>
2020-09-19 17:35:07 -0500

    Updates

---
+++ precache-manifest.ba9fd746fd0109bf9ca878c7207fe014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e812d0264ed944e94aa18722605b1a4b",
+    "url": "/oddisee/index.html"
+  },
+  {
+    "revision": "4321a525b3673e6a2afa",
+    "url": "/oddisee/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "29f5f689622bae1aa9b2",
+    "url": "/oddisee/static/js/2.3e83d6c6.chunk.js"
+  },
+  {
+    "revision": "dade1a2dfd46afb3fc4554c37cbf7c05",
+    "url": "/oddisee/static/js/2.3e83d6c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4321a525b3673e6a2afa",
+    "url": "/oddisee/static/js/main.2576b7ce.chunk.js"
+  },
+  {
+    "revision": "2ba3ea26cfba5ce426f4",
+    "url": "/oddisee/static/js/runtime-main.3765dc79.js"
+  }
+]);
