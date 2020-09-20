5bac47c6f1d33d906a8c399eba12b9453c15105d
John Ottenlips <jottenlips@gmail.com>
2020-09-20 09:55:20 -0500

    add an enemy

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ import {
 import { useKeyPress, useKeyPressEvent } from "react-use";
 import { set } from "ramda";
 const [width, height] = [500, 200];
-const spritesheet = "sprites/oddy.json";
 
 const Oddy = () => {
+  const spritesheet = "sprites/oddy.json";
   const [frames, setFrames] = useState([]);
   // const [rot, setRot] = useState(0);
   const [x, setX] = useState(0);
@@ -44,7 +44,8 @@ const Oddy = () => {
   });
 
   // load
-  useEffect(() => {
+  frames.length === 0 &&
+    !app.loader.loading &&
     app.loader.add(spritesheet).load((_, resource) => {
       setFrames(
         Object.keys(resource[spritesheet].data.frames).map((frame) =>
@@ -52,7 +53,65 @@ const Oddy = () => {
         )
       );
     });
-  }, []);
+
+  console.log(app);
+
+  if (frames.length === 0) {
+    return null;
+  }
+
+  return (
+    <Container x={x} y={y}>
+      <AnimatedSprite
+        animationSpeed={0.5}
+        isPlaying={walking}
+        textures={frames}
+        anchor={0.5}
+      />
+    </Container>
+  );
+};
+
+const EvilBot = () => {
+  const spritesheet = "sprites/evilbot.json";
+  const [frames, setFrames] = useState([]);
+  // const [rot, setRot] = useState(0);
+  const [x, setX] = useState(20);
+  const [y, setY] = useState(20);
+  const [walking, setWalking] = useState(false);
+  const app = useApp();
+
+  const [rightPressed] = useKeyPress("d");
+  const [leftPressed] = useKeyPress("a");
+  const [downPressed] = useKeyPress("s");
+  const [upPressed] = useKeyPress("w");
+
+  useTick(() => {
+    if (leftPressed) {
+      setX(x - 1);
+    }
+    if (rightPressed) {
+      setX(x + 1);
+    }
+    if (downPressed) {
+      setY(y + 1);
+    }
+    if (upPressed) {
+      setY(y - 1);
+    }
+    setWalking(rightPressed || leftPressed || downPressed || upPressed);
+  });
+
+  // load
+  frames.length === 0 &&
+    !app.loader.loading &&
+    app.loader.add(spritesheet).load((_, resource) => {
+      setFrames(
+        Object.keys(resource[spritesheet].data.frames).map((frame) =>
+          Texture.from(frame)
+        )
+      );
+    });
 
   if (frames.length === 0) {
     return null;
@@ -73,6 +132,7 @@ const Oddy = () => {
 const App = () => (
   <Stage width={width} height={height} options={{ backgroundColor: 0xffffff }}>
     <Oddy />
+    <EvilBot />
   </Stage>
 );
 
